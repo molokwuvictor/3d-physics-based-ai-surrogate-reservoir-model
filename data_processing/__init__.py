24b796f30cc39d3aa780828d5b5410d027e80799
molokwuvictor <molokwuvictor@yahoo.com>
2025-07-07 13:39:55 +0100

    Update data_processing/__init__.py with eager imports

--- data_processing/__init__.py
+++ data_processing/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Author: Victor PhD
-# Date: 2025-07-04
+# Author: Victor Molokwu PhD
+# Date: 07/07/2025 13:05
 # 
 """
 Data Processing Package for SRM
@@ -9,3 +9,71 @@ Data Processing Package for SRM
 This package contains modules for processing simulation and reservoir model data.
 """
 
+# Import from data_processing_utils.py
+from .data_processing_utils import (
+    DataSummary,
+    weave_tensors,
+    create_positional_grids,
+    align_and_trim_pair_lists,
+    split_tensor_sequence,
+    slice_statistics,
+    slice_tensor,
+    reshape_and_save_dict,
+    load_dataframe,
+    l1_normalize_excluding_index,
+    print_group_shapes
+)
+
+# Import from srm_data_processing.py
+from .srm_data_processing import (
+    SRMDataProcessor,
+    load_srm_data,
+    validate_split_ratios
+)
+
+# Import from KL_expansion.py
+from .KL_expansion import (
+    generate_kl_log_normal_real_params_3D,
+    plot_realizations_3D,
+    plot_model_3d_grid
+)
+
+# Import from KLE_realization_generator.py
+from .KLE_realization_generator import (
+    KLConfig,
+    NumpyEncoder,
+    generate_full_config_hash,
+    generate_and_save_realizations,
+    create_output_directory,
+    split_realizations
+)
+
+# Import from simulation_data_process_pipeline.py
+from .simulation_data_process_pipeline import (
+    run_pipeline_from_config,
+    save_results,
+    load_results,
+    run_array_pipeline,
+    process_files_in_directory
+)
+
+__all__ = [
+    # Data Processing Utils
+    'DataSummary', 'weave_tensors', 'create_positional_grids', 'align_and_trim_pair_lists',
+    'split_tensor_sequence', 'slice_statistics', 'slice_tensor', 'reshape_and_save_dict',
+    'load_dataframe', 'l1_normalize_excluding_index', 'print_group_shapes',
+    
+    # SRM Data Processing
+    'SRMDataProcessor', 'load_srm_data', 'validate_split_ratios',
+    
+    # KL Expansion
+    'generate_kl_log_normal_real_params_3D', 'plot_realizations_3D', 'plot_model_3d_grid',
+    
+    # KLE Realization Generator
+    'KLConfig', 'NumpyEncoder', 'generate_full_config_hash', 'generate_and_save_realizations',
+    'create_output_directory', 'split_realizations',
+    
+    # Simulation Data Process Pipeline
+    'run_pipeline_from_config', 'save_results', 'load_results', 'run_array_pipeline',
+    'process_files_in_directory'
+]
